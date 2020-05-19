fbd2663e76db8638694c2fa1d9f9591bdc88d664
miguel <miguelpragier@gmail.com>
2020-05-19 11:39:21 -0300

    2nd beta release

--- ipify.go
+++ ipify.go
@@ -1,8 +1,12 @@
 package ipify
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 )
+
+const timeoutSecondsMin = 1
 
 func bodyDispose(r *http.Response) {
 	if r != nil {
@@ -11,16 +15,40 @@ func bodyDispose(r *http.Response) {
 }
 
 // PublicIP retrieves the current public ip from IPIFy https://www.ipify.org/
-func PublicIP() (string, error) {
-	if resp, err := http.Get(`https://api.ipify.org?format=json`); err != nil {
-		return "", err
-	} else {
-		defer bodyDispose(resp)
-
-		var ipify struct {
-			IP string `json:"ip"`
-		}
-
-		return ipify.IP, nil
+func PublicIP(timeoutSeconds int) (string, error) {
+	if timeoutSeconds < timeoutSecondsMin {
+		timeoutSeconds = timeoutSecondsMin
 	}
+
+	cli := &http.Client{Timeout: time.Duration(timeoutSeconds) * time.Second}
+
+	resp, err := cli.Get(`https://api.ipify.org?format=json`)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer bodyDispose(resp)
+
+	var ipify struct {
+		IP string `json:"ip"`
+	}
+
+	if err0 := json.NewDecoder(resp.Body).Decode(&ipify); err0 != nil {
+		return "", err0
+	}
+
+	return ipify.IP, nil
+}
+
+// PublicIPString retrieves the current public ip from IPIFy https://www.ipify.org/
+// The differences to PublicIP are:
+// - PublicIPString ignores all errors and return an empty string if fails
+// - PublicIPString defines arbitratry timeout value of 5 seconds
+func PublicIPString() string {
+	const timeoutSeconds = 5
+
+	ip, _ := PublicIP(timeoutSeconds)
+
+	return ip
 }
